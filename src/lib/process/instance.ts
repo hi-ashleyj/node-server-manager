f38d67123e5cd948df40c59110d2c94a3fc92923
hi-ashleyj <ajor023@gmail.com>
2024-11-06 10:20:27 +1300

    instance work

--- src/lib/process/instance.ts
+++ src/lib/process/instance.ts
@@ -19,23 +19,58 @@ export class ServerInstance {
     private operation?: ProcessWrapper;
     private root: string;
     private info: NodeServerEditable;
-    private params: RuntimeEditable;
+    private params: RuntimeEditable & { auto: boolean };
 
     private is_present = false;
     private is_installed = false;
     private is_built = false;
 
-    constructor(root: string, server: NodeServerEditable, params: RuntimeEditable) {
+    constructor(root: string, server: NodeServerEditable, params: RuntimeEditable & { auto: boolean }) {
         this.root = root;
         this.info = server;
         this.params = params;
+
+        this.check().then(() => {
+            if (this.params.auto) this.params.start();
+        });
     }
 
     async check() {
-        const dir = await unwrappedStat(this.root);
-        const packageJSON = await unwrappedStat(join(this.root, "package.json"));
-        const modules = await unwrappedStat(join(this.root, "node_modules"));
-        const runner = await unwrappedStat(join(this.root, this.info.path));
+        const [ dir, _d ] = await unwrappedStat(this.root);
+        if (!dir || !dir.isDirectory()) {
+            this.is_present = false;
+            this.is_installed = false;
+            this.is_built = false;
+            return;
+        }
+
+        const [ packageJSON, _p ] = await unwrappedStat(join(this.root, "package.json"));
+        const [ modules, _m ] = await unwrappedStat(join(this.root, "node_modules"));
+        const [ runner, _r ] = await unwrappedStat(join(this.root, this.info.path));
+
+        this.is_present = packageJSON !== null && packageJSON.isFile();
+        this.is_installed = modules !== null && modules.isDirectory();
+        this.is_built = runner !== null && runner.isFile();
+    }
+
+    getStatus() {
+
+    }
+
+    async start() {
+
+    }
+
+    async stop() {
+
+    }
+
+    async operate() {
+
+    }
+
+    async script() {
+
     }
 
 }
