c36b2053a399961452bab36f7d96691a1ba3373f
hi-ashleyj <ajor023@gmail.com>
2025-08-23 15:24:12 +1200

    update frontend to new auth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,31 @@
-<script>
-    import { signIn } from "@auth/sveltekit/client"
+<script lang="ts">
+
+    import { page } from "$app/stores";
+    import { goto } from "$app/navigation";
     import encodeHash64 from "crypto-js/enc-base64";
     import sha512 from "crypto-js/sha512";
+
+    let loading = false;
+    let error = "";
+    const login = async () => {
+        loading = true;
+        const res = await fetch("/login", {
+            method: "POST",
+            body: JSON.stringify({ username, password: encodeHash64.stringify(sha512(password)) })
+        });
+        if (res.ok) {
+            const redirect = $page.url.searchParams.get("redirectTo");
+            await goto(redirect ? decodeURIComponent(redirect) : "/");
+        } else {
+            console.log(res.status);
+        }
+    }
+
+    const autosubmit = (e: KeyboardEvent) => {
+        if (e.key.toLowerCase() === "enter") {
+            login();
+        }
+    }
 
     let username = "";
     let password = "";
@@ -16,9 +40,9 @@
         </div>
         <div class="input-group input-group-divider grid-cols-[6rem_1fr]">
             <label class="input-group-shim grid place-items-center" for="password">Password</label>
-            <input name="password" type="password" bind:value={password} />
+            <input name="password" type="password" bind:value={password} on:keydown={autosubmit} />
         </div>
-        <button class="btn variant-filled-primary" on:click={() => signIn('credentials', { username, password: encodeHash64.stringify(sha512(password)) })}>
+        <button class="btn variant-filled-primary" on:click={login}>
             Log in
         </button>
     </div>
