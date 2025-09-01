6dbb3743accdf3ce4404d040d349adc046ef322c
Ashley J <48008893+hi-ashleyj@users.noreply.github.com>
2025-09-01 12:00:06 +1200

    fixes cors issues for stats supply "type": "module" to runtime package bump versions

--- src/lib/package/stats.ts
+++ src/lib/package/stats.ts
@@ -7,14 +7,23 @@ export const stats: () => Handle = () => {
     return async ({ resolve, event }) => {
         const before = performance.now();
         if (event.url.pathname === "/__nsm__/stats") {
+            if (event.request.method === "OPTIONS") {
+                return new Response(null, {
+                    headers: {
+                        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Headers': '*',
+                    }
+                });
+            }
             if (recent.size <= 0) {
-                return new Response(JSON.stringify({ status: "up", requests: 0 }));
+                return new Response(JSON.stringify({ status: "up", requests: 0 }), {headers: { 'Access-Control-Allow-Origin': '*' }});
             }
             const list = [ ...recent.values() ];
             const min = Math.min(...list);
             const max = Math.max(...list);
             const avg = list.reduce((prev, it) => prev + it, 0) / list.length;
-            return new Response(JSON.stringify({ min, max, avg, status: "up", frequency: list.length / 5 }));
+            return new Response(JSON.stringify({ min, max, avg, status: "up", frequency: list.length / 5 }), {headers: { 'Access-Control-Allow-Origin': '*' }});
         }
         const res = await resolve(event)
         const li = ulid();
